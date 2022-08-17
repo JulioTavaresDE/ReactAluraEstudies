9269c892df0fb7c8f00707a5ca8b70c482c3c8be
JulioTavaresDE <108236055+JulioTavaresDE@users.noreply.github.com>
2022-08-17 12:55:42 +0200

    Controlando o formulário

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,10 +2,11 @@ import { type } from '@testing-library/user-event/dist/type';
 import React from 'react';
 import style from './Botao.module.scss';
 
-class Botao extends React.Component <{children?:React.ReactNode}> {
+class Botao extends React.Component <{children?:React.ReactNode,type?:"button" | "submit" | "reset" | undefined}> {
     render() {
+        const { type = "button"} = this.props;
         return (
-            <button className={style.botao}>
+            <button type={type} className={style.botao}>
                {this.props.children}
             </button>   
         )        
