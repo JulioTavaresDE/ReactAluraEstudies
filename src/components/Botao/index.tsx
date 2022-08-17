fa7d967d30a57fd1558a601916f37e7e251cd416
JulioTavaresDE <108236055+JulioTavaresDE@users.noreply.github.com>
2022-08-17 19:58:16 +0200

    Regressiva

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,17 +1,44 @@
+// import React, { ReactFragment } from "react";
+// import style from './Botao.module.scss';
+
+// type Params = {
+//     type?: "button" | "submit" | "reset" | undefined;
+//     children?: ReactFragment | undefined;
+//     onClick?: () => void
+//   }
+
+// const Botao: React.FC<Params> = (props:Params) =>{
+//      const { type = "button", onClick } = props;
+//         return (
+//             <button onClick={onClick} type={props.type} className={style.botao}>
+//              {props.children} 
+//             </button>   
+//         )        
+//     }
+
+
+// export default Botao;
+
+
 import React from 'react';
 import style from './Botao.module.scss';
 
-type Props ={//coloquei o nome de Props mas poderia ser qualquer nome
-    children: string,
-    type?: "button" | "submit" | "reset" | undefined
-  }
+interface Props {
+  type?: "button" | "submit" | "reset" | undefined,
+  onClick?: () => void,
+  children?: React.ReactNode
+}
 
-class Botao extends React.Component <Props> {
-    render():React.ReactNode {
-        return (
-            <button type={this.props.type} className={style.botao}> {this.props.children} </button>   
-        )        
-    }
+function Botao({ onClick, type, children }: Props) {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      className={style.botao}
+    >
+      {children}
+    </button>
+  )
 }
 
 export default Botao;
--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -16,7 +16,17 @@ export default function Cronometro({selecionado}:Props){
     if(selecionado?.tempo){
         setTempo(tempoParaSegundos(selecionado?.tempo))
     }
-    },[selecionado])
+    },[selecionado]);
+
+    function regressiva(contador:number = 0){
+        setTimeout(()=>{
+            if(contador > 0){
+                setTempo(contador - 1);
+                return regressiva(contador -1);
+            }   
+        },1000);
+    }
+
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card que inicia um Cronomerto</p>
@@ -24,7 +34,7 @@ export default function Cronometro({selecionado}:Props){
             <div className={style.relogioWrapper}>
                 <Relogio tempo={tempo}></Relogio>
             </div>
-            <Botao>Comecar!</Botao>
+            <Botao onClick={()=> regressiva(tempo)}>Comecar!</Botao>
         </div>
     )
 }
